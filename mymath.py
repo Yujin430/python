d40ddb03a2d69f5b888c7dd89158c9e21cc2b010
Yujin430 <fhfsp430@gmail.com>
2015-11-17 07:16:57 +0900

    11/17 오전 7시 16분

---
+++ mymath.py
@@ -0,0 +1,17 @@
+#FILE : mymath.py
+mypi = 3.14
+
+def add(a, b):
+    return a + b
+
+def area(r):
+    return mypi * r * r
+print __name__
+if __name__ == "__main__":
+    print add(1,2)
+    print area(4.0)
+#"SHINHWA"
+#a=1
+#print locals().keys()
+
+
---
+++ scriptcyj/module_test.py
@@ -0,0 +1,10 @@
+#file: module_test.py
+def add(a, b):
+    return a + b
+
+def f():
+    print "Python is becoming popular."
+
+if __name__ == "__main__":
+    print add(1, 10)
+    f()
